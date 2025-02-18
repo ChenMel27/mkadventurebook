15e204750090008d6a212a49c0291830af7a846e
Melanie Chen <ychen.melanie@gmail.com>
2025-02-17 19:46:21 -0500

    Home page and links

--- kenny-website/src/App.js
+++ kenny-website/src/App.js
@@ -3,22 +3,36 @@ import "./App.css";
 function App() {
   return (
     <div className="video-container">
+      {/* Navigation Bar */}
+      <nav className="navbar">
+        <h2 className="logo">K&M</h2>
+        <div className="nav-container">
+          <ul className="nav-links">
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#gameplay">Gameplay</a>
+            </li>
+            <li>
+              <a href="#buy">Buy Now</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
       {/* Background Video */}
       <video autoPlay loop muted playsInline className="background-video">
-        <source src={`${process.env.PUBLIC_URL}/kennybg.mp4`} type="video/mp4" />
+        <source
+          src={`${process.env.PUBLIC_URL}/kennybg.mp4`}
+          type="video/mp4"
+        />
         Your browser does not support the video tag.
       </video>
 
-      {/* Navigation Links in Top Right */}
-      <nav className="nav-links">
-        <a href="#map">Map</a>
-        <a href="#todo">To-Do List</a>
-        <a href="#about">About</a>
-      </nav>
-
       {/* Title Page */}
       <div className="overlay">
-        <h1 className="title">Our Adventure Book</h1>
+        <h1 className="title">Adventure Book</h1>
       </div>
     </div>
   );
